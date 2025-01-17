a226265c46942e5408b7a31856e94924d821a7e3
Caio Dorfmam <dasdorfmam@gmail.com>
2025-01-17 09:20:48 -0300

    Criei a 1° versão do projeto, onde será possível verificar a média climática de uma determinada cidade em um período de tempo específico.

---
+++ temperatura.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+dataset = pd.read_csv('GlobalLandTemperaturesByCity.csv', sep=',', encoding='utf-8')
+
+dataset_dates = dataset['dt'] = pd.to_datetime(dataset['dt'])
+
+dataset = dataset.drop(columns=['AverageTemperatureUncertainty', 'Latitude', 'Longitude'])
+dataset = dataset.dropna()
+
+dataset = dataset.rename(columns={'dt': 'Data', 'AverageTemperature': 'Temperatura Média', 'City': 'Cidade', 'Country': 'País'})
+
+def temperatura_media_cidade(dataset, data_inicial, data_final, cidade):
+    
+    if cidade in dataset['Cidade'].values:
+        dataset_final = dataset[
+            (dataset['Data'] >= data_inicial) & 
+            (dataset['Data'] <= data_final) &
+            (dataset['Cidade'] == cidade)
+        ]
+
+        if not dataset_final.empty:
+            
+            temperatura_media = dataset_final['Temperatura Média'].mean()
+            return f'A temperatura média entre o período selecionado foi de {temperatura_media:.2f}°C.'
+        
+        else:
+            return 'Não há dados o suficiente para retornar a média.'
+    
+    else:
+        return 'Cidade não encontrada no dataset.'
+
+data_inicial = '2000-01-01' # Substitua pela data inicial que desejar
+data_final = '2010-12-31' # Substitua pela data final que desejar
+cidade = 'São Paulo' # Substitua pela cidade que desejar
+
+temperatura_calculada = temperatura_media_cidade(dataset, data_inicial, data_final, cidade)
+
+print(temperatura_calculada)
