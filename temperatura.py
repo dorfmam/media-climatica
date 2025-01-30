5ad916668fea621ed0302af586f6cbad28081792
Caio Dorfmam <dasdorfmam@gmail.com>
2025-01-30 01:27:29 -0300

    Adicionada a docstring sobre a função 'temperatura_media_cidade'

--- temperatura.py
+++ temperatura.py
@@ -9,7 +9,36 @@ dataset = dataset.rename(columns={'dt': 'Data', 'AverageTemperature': 'Temperatu
 dataset['Data'] = pd.to_datetime(dataset['Data'])
 
 def temperatura_media_cidade(dataset, data_inicial, data_final, cidade):
-       
+
+    '''
+    
+    Calcula a temperatura média de uma cidade em um período de tempo específico.
+
+    Parâmetros:
+
+    - dataset: DataFrame contendo os dados da tabela;
+    - data_inicial: Data inicial do período de tempo informado;
+    - data_final: Data final do período de tempo informado;
+    - cidade: Nome da cidade selecionada.
+
+    Tipos:
+
+    - dataset: DataFrame;
+    - data_inicial: str (Formato de YYYY-MM-DD);
+    - data_final: str (Formato de YYYY-MM-DD);
+    - cidade: str.
+
+    Retorno:
+
+    Retorna uma string contendo as informações da temperatura média da cidade informada e formatada para duas casas decimais.
+    
+    Em caso de uma cidade não encontrada no dataset, retorna uma mensagem informando que a cidade não foi encontrada.
+
+    Em caso de informações incompletas para o cácula da temperatura, retorna uma mensagem informando que não há dados
+    o suficiente para retorna a média.
+        
+    '''
+
     if cidade in dataset['Cidade'].values:
         dataset_final = dataset[
             (dataset['Data'] >= data_inicial) & 
